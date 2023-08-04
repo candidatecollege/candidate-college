229ecf765143ce5d89054d033db2d8662a1325ec
gustonecrush <codeboxezz@gmail.com>
2023-08-05 06:56:09 +0700

    installing coming soon component to about us and programs page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,19 @@
 'use client'
-import { Footer, Navbar } from '@/components'
+import { ComingSoon, Footer, Navbar } from '@/components'
 import React from 'react'
 
 const About = () => {
   return (
-    <main className="bg-primary h-full">
+    <main className="bg-white h-full">
       {/* Navbar */}
       <Navbar active='About Us' isDetail={false} />
 
+      <ComingSoon />
+
       {/* Footer */}
+      <div className="w-full bg-primary">
       <Footer />
+      </div>
     </main>
   )
 }
