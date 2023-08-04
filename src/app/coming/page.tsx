bc4647ad9acc538c3a169620a4447f1a0eaae172
intgrldxdy <titinmiduknyp@gmail.com>
2023-08-04 09:10:32 +0700

    Fix the page

--- src/app/coming/page.tsx
+++ src/app/coming/page.tsx
@@ -8,18 +8,18 @@ const Coming = () => {
     <main className="bg-white h-full">
       {/* Navbar */}
       <Navbar />
-      
-      <section className="flex flex-col w-full h-full lg:pt-[200px] mx-auto md:pt-[165px] pt-[185px] px-5 py-8 md:max-w-6xl md:mx-auto justify-center md:items-center relative md:h-3/6 lg:h-screen">
+
+      <section className="flex flex-col w-full h-full lg:pt-[140px] mx-auto md:pt-[165px] pt-[185px] px-5 py-8 md:max-w-6xl md:mx-auto justify-center md:items-center relative md:h-3/6 lg:h-screen">
       <Image src={'/decoration/coming.png'} width={0} height={0}
-          className='lg: w-[380px] mx-auto' 
+          className='lg: w-[600px] mx-auto' 
           alt='Coming Soon Decoration'
           title='Coming Soon Decoration'
         />
-        <h1 className="font-semibold text-primary pt-7 text-center text-2xl md:text-[50px] xl:w-[70%] md:w-[90%] md:text-center leading-[50%]">
+        <h1 className="font-semibold text-primary text-center lg:text-[40px] p-1 md:text-[30px] xl:w-[70%] md:w-[90%] md:text-center leading-[50%]">
           Coming Soon
         </h1>
 
-        <p className="text-gray text-sm text-center leading-2 md:pt-6 pt-2 md:text-center lg:text-base md:w-[70%]">
+        <p className="text-gray text-sm text-center leading-2 md:pt-4 pt-2 md:text-center lg:text-base md:w-[70%]">
           This still developing Candidaters! <br />
            Stay tuned
         </p>
