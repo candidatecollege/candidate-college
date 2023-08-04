655f67dad9e2470c79fe65ec3266ee497c12039a
gustonecrush <codeboxezz@gmail.com>
2023-08-05 06:26:00 +0700

    sliced coming page to be component so will be reusable

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CTA from "./CTA";
-import ComingSoon from "./ComingSoon";
 import ListItem from "./ListItem";
 import CardItem from "./CardItem";
 import ComingSoon from "./ComingSoon";
@@ -10,7 +9,6 @@ export {
     Navbar,
     Footer,
     CTA,
-    ComingSoon,
     ListItem,
     CardItem,
     ComingSoon
