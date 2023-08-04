25eb462c8c1fe9f918094a64652365005e116ebb
gustonecrush <codeboxezz@gmail.com>
2023-08-05 06:22:52 +0700

    sliced coming page to be component so will be reusable

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CTA from "./CTA";
+import ComingSoon from "./ComingSoon";
 
 export {
     Navbar,
     Footer,
-    CTA
+    CTA,
+    ComingSoon
 }
