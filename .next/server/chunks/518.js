9ea5467a2caed36b68c20d5513ef0af9be7c692e
gustonecrush <codeboxezz@gmail.com>
2023-08-16 09:29:28 +0700

    uploaded .next

---
+++ .next/server/chunks/518.js
@@ -0,0 +1,39 @@
+"use strict";
+exports.id = 518;
+exports.ids = [518];
+exports.modules = {
+
+/***/ 88518:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(18038);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const SearchIcon = ({ size, color })=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("svg", {
+        xmlns: "http://www.w3.org/2000/svg",
+        width: size,
+        height: size,
+        viewBox: "0 0 24 24",
+        fill: "none",
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("path", {
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+            d: "M4.06176 7.66945C5.48796 4.24481 8.82961 2.01016 12.5393 2.00025C16.5375 1.9792 20.0926 4.54018 21.339 8.33922C22.5853 12.1383 21.2381 16.3075 18.0044 18.6589C14.7707 21.0104 10.3893 21.0069 7.15934 18.6503L4.27934 21.5303C3.98652 21.8227 3.51216 21.8227 3.21934 21.5303C2.92689 21.2374 2.92689 20.7631 3.21934 20.4703L6.00934 17.6803C3.40324 15.0401 2.63556 11.0941 4.06176 7.66945ZM5.39891 14.1692C6.59638 17.0549 9.41508 18.9343 12.5393 18.9303V18.8903C16.7796 18.8848 20.2219 15.4605 20.2493 11.2203C20.2534 8.09599 18.374 5.27729 15.4883 4.07982C12.6026 2.88235 9.27967 3.5422 7.07048 5.75139C4.86129 7.96058 4.20144 11.2835 5.39891 14.1692Z",
+            fill: color
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SearchIcon);
+
+
+/***/ })
+
+};
+;
