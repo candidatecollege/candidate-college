857b7161785f139ca4d369e48ccc9cdc092cbfb6
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-01-24 14:36:34 +0700

    give some change

--- src/app/about/division/[slug]/page.tsx
+++ src/app/about/division/[slug]/page.tsx
@@ -8,7 +8,7 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 
 // Import React State
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 // Import React Router
 import { usePathname } from "next/navigation";
@@ -22,19 +22,21 @@ import CardMember from "../../../../components/CardDivision";
 // About Page
 const Division = (props: any) => {
   const pathName = usePathname();
-  const pathArray = pathName.split('/');
+  const pathArray = pathName.split("/");
   const lastEndPoint = pathArray[pathArray.length - 1];
   const divisionName = formatEndpointText(lastEndPoint);
 
   const [profiles, setProfiles] = useState<Profile[]>([]);
-  
+
   // Fetch Data from API
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
         console.log("Fetching data...");
 
-        const response = await fetch("https://cors-proxy-infinityfree.vercel.app/api/members");
+        const response = await fetch(
+          "https://cors-proxy-infinityfree.vercel.app/api/members"
+        );
         const responseData = await response.json();
 
         console.log("Response status:", response.status);
@@ -44,43 +46,45 @@ const Division = (props: any) => {
           name: item.name,
           position: item.position,
           linkedin: item.linkedin,
-          instagram: item.instagram
+          instagram: item.instagram,
         }));
-  
+
         setProfiles(MembersData);
-      }
-      catch(error) {
+      } catch (error) {
         console.error("Error fetching data from API : ", error);
       }
     };
 
-    fetchData(); 
+    fetchData();
   }, []);
 
   // Setting Layout Card
   const renderCards = () => {
     const cardsPerRow = [2, 4, 4];
     let currentIndex = 0;
-    
+
     return cardsPerRow.map((numCards, rowIndex) => {
-      const rowCards = profiles.slice(currentIndex, currentIndex + numCards)
+      const rowCards = profiles.slice(currentIndex, currentIndex + numCards);
       currentIndex += numCards;
 
       return (
-        <div key={rowIndex} className="flex lg:justify-center lg:items-centers lg:flex-row xsm:flex-col xxsm:flex-col">
+        <div
+          key={rowIndex}
+          className="flex lg:justify-center lg:items-centers lg:flex-row xsm:flex-col xxsm:flex-col"
+        >
           {rowCards.map((profile) => (
             <div className="lg:mx-0 xsm:mx-auto xxsm:mx-auto">
               <CardMember
-               img={`https://cors-proxy-infinityfree.vercel.app/uploads/${profile.img}`}
-               name={profile.name}
-               position={profile.position}
-               linkedin={profile.linkedin}
-               instagram={profile.instagram}
-               />
+                img={`https://cors-proxy-infinityfree.vercel.app/uploads/${profile.img}`}
+                name={profile.name}
+                position={profile.position}
+                linkedin={profile.linkedin}
+                instagram={profile.instagram}
+              />
             </div>
           ))}
         </div>
-      );  
+      );
     });
   };
 
@@ -95,17 +99,16 @@ const Division = (props: any) => {
           {divisionName}
         </h1>
         <p className="text-gray text-sm leading-7 lg:pt-8 xsm:pt-7 xxsm:pt-6 max-w-[600px] mx-auto text-center">
-          {props.description} 
+          {props.description}
         </p>
         <br />
       </section>
 
       {/* Card */}
       <div className="flex flex-col bg-zinc-100">
-        <div className="flex flex-col -mt-[380px] z-40">
-          {renderCards()}
-        </div>
-        <br /><br />
+        <div className="flex flex-col -mt-[380px] z-40">{renderCards()}</div>
+        <br />
+        <br />
       </div>
 
       {/* CTA */}
@@ -116,16 +119,20 @@ const Division = (props: any) => {
         <Footer />
       </div>
     </main>
-  )
+  );
 };
 
 // Change Format of Endpoint to Uppercase
 const formatEndpointText = (endpoint: any) => {
-  return endpoint.split("-").map((word: any) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-}
+  return endpoint
+    .split("-")
+    .map((word: any) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
 
 Division.defaultProps = {
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim"
-}
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+};
 
 export default Division;
