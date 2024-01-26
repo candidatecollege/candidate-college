f96e57aadcf39ffc45f444aa454032ad2801ebe1
Haikal Rafif <99420525+haikalrafifas@users.noreply.github.com>
2024-01-26 10:21:18 +0700

    Update src/components/GoogleAnalytics.tsx
    Making changes to src/components/GoogleAnalytics.tsx to update the gtag value for a new Google Analytics account.

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -4,14 +4,14 @@ import Script from "next/script";
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-M41SF38MQ6" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-91CV4XDFKH" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'G-M41SF38MQ6');
+          gtag('config', 'G-91CV4XDFKH');
         `}
       </Script>
     </>
