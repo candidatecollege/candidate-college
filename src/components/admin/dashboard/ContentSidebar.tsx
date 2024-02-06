4ac14865cbd628b1774a195788430461fa339ff6
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-02-06 19:34:40 +0700

    Complete testing

--- src/components/admin/dashboard/ContentSidebar.tsx
+++ src/components/admin/dashboard/ContentSidebar.tsx
@@ -25,50 +25,52 @@ export default function ContentSidebar() {
       title: "Candidates",
     },
     {
-  path: "/careers/admin/dashboard/profile",
-  regex: /^\/careers\/admin\/dashboard\/profile\/?$/i,
-  icon: (
-    <ProfileSvg
-      stroke={`${
-        pathname.match(/^\/careers\/admin\/dashboard\/profile\/?$/i) || pathname === "/careers/admin/candidates/profile"
-          ? "#1B4E6B"
-          : "white"
-      }`}
-    />
-  ),
-  title: "Profile",
-},
-{
-  path: "/careers/admin/dashboard/le",
-  regex: "gsdagasd",
-  icon: (
-    <ClipboardSvg
-      stroke={`${
-        pathname === "/careers/admin/dashboard/le" ? "#1B4E6B" : "white"
-      }`}
-    />
-  ),
-  title: "Status",
-},
-];
+      path: "/careers/admin/dashboard/profile",
+      regex: /^\/careers\/admin\/dashboard\/profile\/?$/i,
+      icon: (
+        <ProfileSvg
+          stroke={`${
+            pathname.match(/^\/careers\/admin\/dashboard\/profile\/?$/i) ||
+            pathname === "/careers/admin/candidates/profile"
+              ? "#1B4E6B"
+              : "white"
+          }`}
+        />
+      ),
+      title: "Profile",
+    },
+    {
+      path: "/careers/admin/dashboard/le",
+      regex: "gsdagasd",
+      icon: (
+        <ClipboardSvg
+          stroke={`${
+            pathname === "/careers/admin/dashboard/le" ? "#1B4E6B" : "white"
+          }`}
+        />
+      ),
+      title: "Status",
+    },
+  ];
 
-return urlPath.map(({ path, icon, title, regex }, index) => {
-  return (
-    <Link
-      key={index}
-      href={path}
-      className={`flex items-center gap-4 cursor-pointer ${
-        pathname.match(regex) ? "bg-secondary" : ""
-      } px-4 py-3 rounded-3xl`}
-    >
-      {icon}
-      <h2
-        className={`text-[14px]  ${
-          pathname.match(regex) ? "text-primary" : "text-white"
-        } leading-[20px]`}
+  return urlPath.map(({ path, icon, title, regex }, index) => {
+    return (
+      <Link
+        key={index}
+        href={path}
+        className={`flex items-center gap-4 cursor-pointer ${
+          pathname.match(regex) ? "bg-secondary" : ""
+        } px-4 py-3 rounded-3xl`}
       >
-        {title}
-      </h2>
-    </Link>
-  );
-});
+        {icon}
+        <h2
+          className={`text-[14px]  ${
+            pathname.match(regex) ? "text-primary" : "text-white"
+          } leading-[20px]`}
+        >
+          {title}
+        </h2>
+      </Link>
+    );
+  });
+}
